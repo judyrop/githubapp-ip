207fc47919ae2640c38c199b36f1d00596abf12d
Your Name <youremail@domain.com>
2021-08-29 11:53:08 +0300

    added functionalities in the component

--- GithubWeb/src/app/app-routing.module.ts
+++ GithubWeb/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { GithubComponent } from './github/github.component';
+import { AboutComponent } from './about/about.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'github', component: GithubComponent},
+  { path: 'about', component: AboutComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
